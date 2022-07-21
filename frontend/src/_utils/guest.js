384d7baf05755d11e9ab34dc473068bb847f99a4
Adam Thometz <adamthometz@gmail.com>
2022-07-21 13:26:28 -0400

    added getGuests to api

--- frontend/src/_utils/guest.js
+++ frontend/src/_utils/guest.js
@@ -3,28 +3,33 @@ import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
 
 class Guest {
-  static async request(endpoint, data) {
+  static async request(endpoint, data, method) {
     const url = `${BASE_URL}/${endpoint}`;
     try {
-      const res = await axios.post(url, data)
+      const res = await axios({ url, data, method });
       return res.data;
     } catch (e) {
       throw e.response.data.error.message;
     };
   };
+
+  static async getGuests() {
+    const res = await this.request('guests', {}, 'get');
+    return res.guests;
+  };
   
   static async checkGuest(data) {
-    const res = await this.request('check', data);
+    const res = await this.request('check', data, 'post');
     return res.guest;
   };
 
   static async markNotComing(data) {
-    const res = await this.request('not-coming', data);
+    const res = await this.request('not-coming', data, 'post');
     return res.guest;
   };
 
   static async markComing(data) {
-    const res = await this.request('coming', data);
+    const res = await this.request('coming', data, 'post');
     return res.guest;
   };
 };
